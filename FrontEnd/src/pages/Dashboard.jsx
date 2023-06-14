ad76594585325337ddb2fa21dac1b64ed28991ff
Ihjazul Aslam AT <ihjazmohd19@gmail.com>
2023-06-15 01:39:53 +0530

    connected apis of adding staff ,register manager

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -1,79 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './a.css';
 import Sidebar from '../components/Sidebar';
-const Dashboard= () => {
-    return (
-        <Sidebar name1="Dashboard" name2="ManageStaff" name3="ViewReportedIssue" name4="ManageContatcs" name5="ManageProducts" name6="ViewChangeRequest" name7="ViewRankList">
-        <div>
-        
-            <div class="a">
-                <h1 class="h1" >Dashboard</h1>
-                <input type="date"></input>
-                <input type="search"></input>
-                <button>search</button>
-            </div>
-            <table border={5} width={1200} cellPadding={20}>
-                <tr  height={30}>
-                    <th>Staff name</th>
-                    <th>Designation</th>
-                    <th>email</th>
-                    <th>Total</th>
-                    <th>Finished</th>
-                    <th>Not Finished</th>
-                </tr>
-                <tr height={70} >
-                    <td>dhuyoof</td>
-                    <td>manager</td>
-                    <td>dhoop@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr height={70}>
-                    <td>ihjaz</td>
-                    <td>Asst.manager</td>
-                    <td>ihjaz@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr height={70}>
-                    <td>angel</td>
-                    <td>team leader</td>
-                    <td>angel@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr height={70}>
-                    <td>thanveer</td>
-                    <td>team coordinator</td>
-                    <td>tha@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr height={70}>
-                    <td>robin</td>
-                    <td>manager</td>
-                    <td>dhossp@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-                <tr height={70}>
-                    <td>unni</td>
-                    <td>team member</td>
-                    <td>unni@asd</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                </tr>
-            </table>
+import { publicGateway } from '../services/gateway';
 
+const Dashboard = () => {
+  const [staffArray, setStaffArray] = useState([]);
+
+  useEffect(() => {
+    publicGateway
+      .post('/manager/ViewStaffs')
+      .then((res) => {
+        console.log(res.data);
+        setStaffArray(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <Sidebar
+      name1="Dashboard"
+      name2="ManageStaff"
+      name3="ViewReportedIssue"
+      name4="ManageContatcs"
+      name5="ManageProducts"
+      name6="ViewChangeRequest"
+      name7="ViewRankList"
+    >
+      <div>
+        <div className="a">
+          <h1 className="h1">Dashboard</h1>
+          <input type="date" />
+          <input type="search" />
+          <button>search</button>
         </div>
-        </Sidebar>
-    );
+        <table border={5} width={1200} cellPadding={20}>
+          <tr height={30}>
+            <th>Staff name</th>
+            <th>Designation</th>
+            <th>Email</th>
+            <th>Total</th>
+            <th>Finished</th>
+            <th>Not Finished</th>
+          </tr>
+          {staffArray.map((staff, index) => (
+            <tr key={index} height={70}>
+              <td>{staff.Name}</td>
+              <td>{staff.Designation}</td>
+              <td>{staff.Email}</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </Sidebar>
+  );
 };
 
 export default Dashboard;
--- FrontEnd/src/pages/ManageStaff.jsx
+++ FrontEnd/src/pages/ManageStaff.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 import { NavLink } from 'react-router-dom';
+import { publicGateway } from '../services/gateway';
+
 
 import './b.css';
 
 const Managestaff = () => {
+    const [staffArray, setStaffArray] = useState([]);
+
+    useEffect(() => {
+      publicGateway
+        .post('/manager/ViewStaffs')
+        .then((res) => {
+          console.log(res.data);
+          setStaffArray(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+  
     return (
         <Sidebar name1="Dashboard" name2="ManageStaff" name3="ViewReportedIssue" name4="ManageContatcs" name5="ManageProducts" name6="ViewChangeRequest" name7="ViewRankList">
         
@@ -26,64 +42,18 @@ const Managestaff = () => {
                     <th>Designation</th>
                     <th>Staff Id</th>
                     <th>email</th>
-                    <th>Country code</th>
                     <th>phone number</th>
-                    <th>action</th>
                 </tr>
-                <tr height={70}>
-                    <td>1</td>
-                    <td>dhuyoof</td>
-                    <td>manager</td>
-                    <td>353</td>
-                    <td>dhoop@asd</td>
-                    <td>91</td>
-                    <td>7388228812</td>
-                </tr>
-                <tr height={70}>
-                    <td>2</td>
-                    <td>ihjaz</td>
-                    <td>Asst.manager</td>
-                    <td>363</td>
-                    <td>ihjaz@asd</td>
-                    <td>91</td>
-                    <td>9888228812</td>
-                </tr>
-                <tr height={70}>
-                    <td>3</td>
-                    <td>angel</td>
-                    <td>Team leader</td>
-                    <td>53</td>
-                    <td>angel@asd</td>
-                    <td>91</td>
-                    <td>7388228887</td>
-                </tr>
-                <tr height={70}>
-                    <td>4</td>
-                    <td>thanveer</td>
-                    <td>Team coordinator</td>
-                    <td>153</td>
-                    <td>tha@asd</td>
-                    <td>91</td>
-                    <td>6288228812</td>
-                </tr>
-                <tr height={70}>
-                    <td>5</td>
-                    <td>robin</td>
-                    <td>manager</td>
-                    <td>523</td>
-                    <td>dhossp@asd</td>
-                    <td>91</td>
-                    <td>8647228812</td>
-                </tr>
-                <tr height={70}>
-                    <td>6</td>
-                    <td>unni</td>
-                    <td>team member</td>
-                    <td>300</td>
-                    <td>unni@asd</td>
-                    <td>91</td>
-                    <td>7388228812</td>
-                </tr>
+                {staffArray.map((staff, index) => (
+            <tr key={index} height={70}>
+              <td>{index+1}</td>
+              <td>{staff.Name}</td>
+              <td>{staff.Designation}</td>
+              <td>{staff.DocId}</td>
+              <td>{staff.Email}</td>
+              <td>{staff.PhoneNo}</td>
+            </tr>
+          ))}
 
             </table>
         </div>
