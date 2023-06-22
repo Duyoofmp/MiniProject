fbe79daf213939699c08737c0d8f08532be6a3f3
Duyoofmp <duyoofibnudileep@gmail.com>
2023-06-22 14:05:47 +0530

    back

--- BackEnd/functions/Services/Reports.js
+++ BackEnd/functions/Services/Reports.js
@@ -8,35 +8,21 @@ const moment = require('moment-timezone')
 
 async function Create(req, res) {
     req.body.index = Date.now()
-  
-    const check = await dataHandling.WhereGet("Report", "Name", req.body.Name);
-    if (check) {
+    req.body.Date=new Date().toLocaleDateString('en-GB');
       await dataHandling.Create("Report", req.body)
       return res.json(true)
-    } else {
-      return res.json("Report with same name already exists !")
-  
-    }
-  
   }
   async function Update(req, res) {
-    req.body.index = Date.now();
-    const check = await dataHandling.WhereGet("Report", "Namw", req.body.Name, req.body.DocId);
-    if (check) {
+    
       await dataHandling.Update("Report", req.body, req.body.DocId)
       return res.json(true)
-    } else {
-      return res.json("Report with same Name already exists !")
-    }
+   
   }
   async function Delete(req, res) {
-    const lead = await db.collection("Lead").where("ReportId", "==", req.body.DocId).limit(1).get();
-    if (lead.size === 0) {
+    
       await dataHandling.Delete("Report", req.body.DocId)
       return res.json(true)
-    } else {
-      return res.json("Cannot Delete This Report At This Moment :(")
-    }
+    
   
   }
   
@@ -45,36 +31,10 @@ async function Create(req, res) {
     return res.json(data)
   }
 
-  
-async function AssignProduct(req, res) {
-    const temp = [];
-    const today = moment().tz('Asia/Kolkata');
-    
-        req.body.ContactIds.forEach(Contactid => {
-            temp.push(db.collection("Leads").doc(Contactid + "_" + req.body.ProductId).set({StaffId: req.body.StaffId, Status: "Open", index: Date.now(), Date: today.format('YYYY-MM-DD')}, { "merge": true }))
-        })
-    
 
-    await Promise.all(temp)
-    return res.json(true)
-}
-
-async function SetAStatus(req, res) {
-    const temp = [];
-    db.collection("Leads").doc(req.body.LeadId).update({ "Status": req.body.Status })
-        .then(snap => {
-            return res.json(true);
-        })
-        .catch(err => {
-            functions.logger.error(err);
-            return res.json(false);
-        })
-}
   module.exports = {
     Create,
     Update,
     Delete,
-    Read,
-    SetAStatus,
-    AssignProduct
+    Read
   }
