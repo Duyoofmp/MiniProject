2897d3bba590e8fbfd8c2996d8e9cce9bf172c1d
thenveerpk <thanveerpk912@gmail.com>
2023-06-22 15:22:43 +0530

    staffproduct page

--- FrontEnd/src/pages/StaffProduct.jsx
+++ FrontEnd/src/pages/StaffProduct.jsx
@@ -1,98 +1,38 @@
 import React from 'react';
 import StaffSidebar from '../components/StaffSidebar';
+import { useNavigate } from 'react-router-dom';
 
+function StaffProduct({ product }) {
+  const navigate = useNavigate();
 
+  const navigateToPage = (productObj) => {
+    navigate('/staffDashboard', { state: productObj });
+  };
 
-function staffProduct({ product }) {
   return (
     <StaffSidebar>
-    <div className="product-page">
-       <table border={5} width={1000} cellPadding={20}>
-                <tr height={70}>
-                    <th>Number</th>
-                    <th>name</th>
-                    <th>Id</th>
-                    <th>Description</th>
-                    <th>Action</th>
-                </tr>
-                <tr height={50}>
-                    <td>1</td>
-                    <td>Vlsi banglore</td>
-                    <td>vlsi banglore</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>2</td>
-                    <td>Vlsi kerala</td>
-                    <td>vlsi kerala</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>3</td>
-                    <td>banking</td>
-                    <td>banking</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>4</td>
-                    <td>finance</td>
-                    <td>finance</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>5</td>
-                    <td>fitness online chennai</td>
-                    <td>fitness online chennai</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>6</td>
-                    <td>fitness online mumbai</td>
-                    <td>fitness online mumbai</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>7</td>
-                    <td>sports centre</td>
-                    <td>sports centre</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>8</td>
-                    <td>gym centre delhi</td>
-                    <td>gym centre delhi</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>9</td>
-                    <td>gym centre banglore</td>
-                    <td>gym centre banglore</td>
-                    <td>nil</td>
-
-                </tr>
-                <tr height={50}>
-                    <td>10</td>
-                    <td>lab goa</td>
-                    <td>lab goa</td>
-                    <td>nil</td>
-
-                </tr>
-                 
-            </table>
-
-    </div>
+      <div className='staff'>
+        <h1 className='h2'>Products</h1>
+      </div>
+      <div className="product-page">
+        <table border={5} width={1000} cellPadding={20}>
+          <tr height={70}>
+            <th>Number</th>
+            <th>Name</th>
+            <th>Id</th>
+            <th>View product</th>
+          </tr>
+          <tr height={50}>
+            <td>1</td>
+            <td>Vlsi banglore</td>
+            <td>vlsi banglore</td>
+            <td><button onClick={() => navigateToPage(product)}>click</button></td>
+          </tr>
+          {/* Rest of the table rows */}
+        </table>
+      </div>
     </StaffSidebar>
   );
 }
 
-export default staffProduct;
-
-
+export default StaffProduct;
