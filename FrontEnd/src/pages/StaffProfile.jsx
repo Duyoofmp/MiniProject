fcdfb5e271c9571d08164d5d14a2993b2feebe1f
Ihjazul Aslam AT <ihjazmohd19@gmail.com>
2023-08-09 13:18:20 +0530

    profile

--- FrontEnd/src/pages/StaffProfile.jsx
+++ FrontEnd/src/pages/StaffProfile.jsx
@@ -1,140 +1,155 @@
-import React, {useState} from 'react';
-import './StaffProfile.css'
+import React, { useState } from 'react';
+import './StaffProfile.css';
 import StaffSidebar from '../components/StaffSidebar';
 
 const Profile = () => {
-    const [name, setName] = useState('John Doe');
-    const [bio, setBio] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-    const [email, setEmail] = useState('john.doe@example.com');
-    const [location, setLocation] = useState('New York, USA');
-    const [website, setWebsite] = useState('https://www.example.com');
-    const [profilePicture, setProfilePicture] = useState('https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80');
-    const [isEditMode, setIsEditMode] = useState(false);
-  
-    const handleNameChange = (e) => {
-      setName(e.target.value);
+  const [name, setName] = useState('John Doe');
+  const [designation, setDesignation] = useState('Software Engineer');
+  const [email, setEmail] = useState('john.doe@example.com');
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('********');
+  const [phoneNumber, setPhoneNumber] = useState('123-456-7890');
+  const [profileImage, setProfileImage] = useState('');
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleDesignationChange = (e) => {
+    setDesignation(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleProfileImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setProfileImage(reader.result);
     };
-  
-    const handleBioChange = (e) => {
-      setBio(e.target.value);
-    };
-  
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value);
-    };
-  
-    const handleLocationChange = (e) => {
-      setLocation(e.target.value);
-    };
-  
-    const handleWebsiteChange = (e) => {
-      setWebsite(e.target.value);
-    };
-  
-    const handleProfilePictureChange = (e) => {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-  
-      reader.onloadend = () => {
-        setProfilePicture(reader.result);
-      };
-  
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-    };
-  
-    const handleEditClick = () => {
-      setIsEditMode(true);
-    };
-  
-    const handleSaveClick = () => {
-      setIsEditMode(false);
-      // Save the updated profile information here
-    };
-  
-    return (
-      <StaffSidebar>
-        <div className="profile">
-          <div className="profile-picture-container">
-            {isEditMode ? (
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleProfilePictureChange}
-              />
-            ) : (
-              <img
-                className="profile-picture"
-                src={profilePicture}
-                alt="Profile Picture"
-              />
-            )}
-          </div>
-          <h1 className="profile-name">
-            {isEditMode ? (
-              <input
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-              />
-            ) : (
-              name
-            )}
-          </h1>
-          <p className="profile-bio">
-            {isEditMode ? (
-              <textarea value={bio} onChange={handleBioChange} />
-            ) : (
-              bio
-            )}
-          </p>
-          <ul className="profile-details">
-            <li>
-              <strong>Email:</strong>{' '}
-              {isEditMode ? (
-                <input
-                  type="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                />
-              ) : (
-                email
-              )}
-            </li>
-            <li>
-              <strong>Location:</strong>{' '}
-              {isEditMode ? (
-                <input
-                  type="text"
-                  value={location}
-                  onChange={handleLocationChange}
-                />
-              ) : (
-                location
-              )}
-            </li>
-            <li>
-              <strong>Website:</strong>{' '}
-              {isEditMode ? (
-                <input
-                  type="url"
-                  value={website}
-                  onChange={handleWebsiteChange}
-                />
-              ) : (
-                <a href={website}>{website}</a>
-              )}
-            </li>
-          </ul>
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleShowPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleEditClick = () => {
+    setIsEditMode(true);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditMode(false);
+    // Save the updated profile information here
+  };
+
+  const defaultProfileImage =
+    'https://www.366icons.com/media/01/profile-avatar-account-icon-16699.png'; 
+
+  return (
+    <StaffSidebar>
+      <div className="profile">
+        <div className="profile-picture-container">
           {isEditMode ? (
-            <button className="save-button" onClick={handleSaveClick}>Save</button>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleProfileImageChange}
+            />
           ) : (
-            <button className="edit-button" onClick={handleEditClick}>Edit Profile</button>
+            <img
+              className="profile-picture"
+              src={profileImage || defaultProfileImage}
+              alt="Profile Picture"
+            />
           )}
         </div>
-        </StaffSidebar>
-    );
-  };
-  
-  export default Profile;
+        <h1 className="profile-name">
+          {isEditMode ? (
+            <input type="text" value={name} onChange={handleNameChange} />
+          ) : (
+            name
+          )}
+        </h1>
+        <p className="profile-designation">
+          {isEditMode ? (
+            <input
+              type="text"
+              value={designation}
+              onChange={handleDesignationChange}
+            />
+          ) : (
+            designation
+          )}
+        </p>
+        <ul className="profile-details">
+          <li>
+            <strong>Email:</strong>{' '}
+            {isEditMode ? (
+              <input type="email" value={email} onChange={handleEmailChange} />
+            ) : (
+              email
+            )}
+          </li>
+          <li>
+            <strong>Password:</strong>{' '}
+            {isEditMode ? (
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            ) : (
+              '******'
+            )}
+            {isEditMode && (
+              <input
+                type="checkbox"
+                checked={showPassword}
+                onChange={handleShowPasswordToggle}
+              />
+            )}
+          </li>
+          <li>
+            <strong>Phone Number:</strong>{' '}
+            {isEditMode ? (
+              <input
+                type="tel"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+              />
+            ) : (
+              phoneNumber
+            )}
+          </li>
+        </ul>
+        {isEditMode ? (
+          <button className="save-button" onClick={handleSaveClick}>
+            Save
+          </button>
+        ) : (
+          <button className="edit-button" onClick={handleEditClick}>
+            Edit Profile
+          </button>
+        )}
+      </div>
+    </StaffSidebar>
+  );
+};
+
+export default Profile;
